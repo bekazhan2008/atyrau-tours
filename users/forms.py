be44923352edd4f926e1055aab3d83527384f7b9
funger-fx <atyraucity84@gmail.com>
2026-01-29 03:20:41 +0500

    added the folder users

---
+++ users/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.ModelForm):
+    username = forms.CharField(max_length=150)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get('password1')
+        p2 = cleaned_data.get('password2')
+
+        if p1 and p2 and p1 != p2:
+            raise forms.ValidationError('Пароли не совпадают')
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('player', 'Участник'),
+        ('organizer', 'Организатор'),
+        ('admin', 'Админ'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='player')
+    city = models.CharField(max_length=100, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
