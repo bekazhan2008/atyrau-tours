7df62aed9d4f510892ca95a513375619036b4fd6
funger-fx <atyraucity84@gmail.com>
2026-01-29 03:57:19 +0500

    Finish

--- users/forms.py
+++ users/forms.py
@@ -10,18 +10,26 @@ class LoginForm(forms.Form):
 
 
 class RegisterForm(forms.ModelForm):
-    username = forms.CharField(max_length=150)
-    password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=150, )
+    password1 = forms.CharField(widget=forms.PasswordInput, label='Пороль')
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Повторите пороль')
+    phone = forms.CharField(widget=forms.TextInput, required=False, label='Номер телефона')
+    email = forms.EmailField(required=False, label='Email')
 
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'phone')
 
     def clean(self):
         cleaned_data = super().clean()
         p1 = cleaned_data.get('password1')
         p2 = cleaned_data.get('password2')
+        email = cleaned_data.get('email')
+        phone = cleaned_data.get('phone')
+
+        if not email and not phone:
+            raise forms.ValidationError('Вы должны ввести либо номер телефона или Email')
+
 
         if p1 and p2 and p1 != p2:
             raise forms.ValidationError('Пароли не совпадают')
--- users/models.py
+++ users/models.py
@@ -11,6 +11,7 @@ class User(AbstractUser):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='player')
     city = models.CharField(max_length=100, blank=True)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.username
