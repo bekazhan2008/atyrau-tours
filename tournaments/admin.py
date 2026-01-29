58e0925a8edf79ab89f1463f94f34a5e7d2620f5
bekazhan2008 <egimbaibeka@gmail.com>
2026-01-29 17:26:04 +0500

    Закончил бэкенд турниров

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Game, Location, Tournament
 
 # Register your models here.
+
+admin.site.register(Game)
+admin.site.register(Location)
+admin.site.register(Tournament)
