d968034897f217ae1ec5eb2d5a5edc21075fd02c
funger-fx <atyraucity84@gmail.com>
2026-01-30 20:42:34 +0500

    combined the phone and email input fields into one

--- users/forms.py
+++ users/forms.py
@@ -11,23 +11,22 @@ class LoginForm(forms.Form):
 
 class RegisterForm(forms.ModelForm):
     username = forms.CharField(max_length=150, )
+    login = forms.CharField(widget=forms.TextInput, label='Введите Email или номер телефона')
     password1 = forms.CharField(widget=forms.PasswordInput, label='Пороль')
     password2 = forms.CharField(widget=forms.PasswordInput, label='Повторите пороль')
-    phone = forms.CharField(widget=forms.TextInput, required=False, label='Номер телефона')
-    email = forms.EmailField(required=False, label='Email')
+
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'phone')
+        fields = ('username', 'login')
 
     def clean(self):
         cleaned_data = super().clean()
         p1 = cleaned_data.get('password1')
         p2 = cleaned_data.get('password2')
-        email = cleaned_data.get('email')
-        phone = cleaned_data.get('phone')
+        login = cleaned_data.get('login')
 
-        if not email and not phone:
+        if not login:
             raise forms.ValidationError('Вы должны ввести либо номер телефона или Email')
 
 
