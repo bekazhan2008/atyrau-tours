37335294a0e3de8e9a2bc2b32df64a02f701280d
bekazhan2008 <egimbaibeka@gmail.com>
2026-02-08 17:50:51 +0500

    EZE

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register_view, name='register'),
     path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('logout/', views.user_logout, name='user_logout'),
     path('profile/', views.profile, name='profile'),
 ]
