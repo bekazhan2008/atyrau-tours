e625ee35c2857361ae973c2c3534513204b36d51
bekazhan2008 <egimbaibeka@gmail.com>
2026-02-12 23:50:02 +0500

    Big update

--- news/models.py
+++ news/models.py
@@ -19,3 +19,12 @@ class News(models.Model):
     def __str__(self):
         return self.title
 
+class MainNews(models.Model):
+    title = models.CharField("Заголовок", max_length=200)
+    img = models.ImageField("Изображение", upload_to='static/img/', null=True, blank=True)
+    text = models.TextField("Текст новости")
+    created_at = models.DateTimeField("Дата публикации", auto_now_add=True)
+    is_published = models.BooleanField("Опубликовано", default=True)
+
+    def __str__(self):
+        return self.title
--- news/views.py
+++ news/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import News
+from .models import News, MainNews
 
 def news(request):
     news = News.objects.all()
-    return render(request, 'news/news.html', {'news': news})
+    main_news = MainNews.objects.filter(is_published=True).order_by('-created_at').first()
+    return render(request, 'news/news.html', {'news': news, 'main_news': main_news})
 
