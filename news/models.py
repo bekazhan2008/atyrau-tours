89a20eec220034321262481bb833e45c0ab9bc8b
bekazhan2008 <egimbaibeka@gmail.com>
2026-02-12 18:35:58 +0500

    tiny update

--- news/models.py
+++ news/models.py
@@ -10,7 +10,7 @@ class News(models.Model):
     ]
 
     title = models.CharField("Заголовок", max_length=200)
-    img = models.ImageField("Изображение", upload_to='news_images/', null=True, blank=True)
+    img = models.ImageField("Изображение", upload_to='static/img/', null=True, blank=True)
     type = models.CharField("Тип новости", max_length=50, choices=TYPE_CHOISES)
     text = models.TextField("Текст новости")
     created_at = models.DateTimeField("Дата публикации", auto_now_add=True)
